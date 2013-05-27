f724d8fd2b45974e3bacf0fb2f9d42bc5be54282
Ilya <123>
2013-05-27 12:35:20 +0300

    Added levels

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -15,6 +15,7 @@ public class GameController : MonoBehaviour {
 	private float lastGenerationTime = -10;
 	private float level = 1;
 	private float bagelsCount = 0;
+	private int bagelCathed = 0;
 	private float missedBagels = 0;
 	
 	private float yDragLevel = 2;
@@ -23,26 +24,59 @@ public class GameController : MonoBehaviour {
 	
 	private bool isPaused = false;
 	
+	private float showNextLevel = 0;
+	
+	
+	
 	// Use this for initialization
 	void Start () {
 		instance = this;
 		bagelPrefab = Instantiate(Resources.Load("Prefabs/Bagel") as GameObject) as GameObject;
 		bagelPrefab.transform.position = new Vector3(100, 0, 0);
 		bagelPrefab.name = "BagelPrefab";
+		bagelPrefab.tag = "Untagged";
 		bagelPrefab.rigidbody.isKinematic = true;
+	}
+	
+	private void IncreaseLevel() {
+		
+		if (level == Settings.maxLevel) level --;
+		level++;
+		bagelCathed = 0;
+		bagelsCount = 0;
+		bagels.Clear();
+		foreach(GameObject bagel in GameObject.FindGameObjectsWithTag("Bagel")) {
+			Destroy(bagel);
+		}
+		showNextLevel = 1f;
+	}
+	
+	private int AvailableBagelTypes() {
+	
+		int result = 2;
+		for (int i = 0; i < Settings.levelToTypes.Length; i++) if (level >= Settings.levelToTypes[i]) result = i+3;
+		return result;
 	}
 	
 	void BagelInBox (string boxAndBagelName) {	//	boxAndBagelName[0] - box first letter, boxAndBagelName[1] - bagel first letter, 
 		//Debug.Log(Time.time.ToString() + " :: " + boxAndBagelName);
 		if (boxAndBagelName[0] == boxAndBagelName[1]) {
+				
+			bagelCathed++;
+			
 			switch (boxAndBagelName[0]) {
 				case '1': score += Settings.boxAScore; break;
 				case '2': score += Settings.boxBScore; break;
 				case '3': score += Settings.boxCScore; break;
 				case '4': score += Settings.boxDScore; break;
 				case '5': score += Settings.boxEScore; break;
-				case 'M': score += Settings.missPenalty; break;
+				case 'M': {
+					score += Settings.missPenalty; 
+					bagelCathed--;
+					break;
+				}
 			}
+			if (bagelCathed>=Settings.bagelsToLevel) IncreaseLevel();
 		} else {
 			score += Settings.missPenalty;
 			missedBagels++;
@@ -56,9 +90,10 @@ public class GameController : MonoBehaviour {
 	
 	void CreateBagel () {
 		GameObject bagel = Instantiate(bagelPrefab) as GameObject;
+		bagel.tag = "Bagel";
 		bagel.transform.position = new Vector3(Random.value - 0.5f, 0, 2);
 		bagel.rigidbody.isKinematic = false;
-		int bagelType = Random.Range(1, 6);
+		int bagelType = Random.Range(1,AvailableBagelTypes()+1);
 		bagel.renderer.material.mainTexture = Resources.Load("Png/" + bagelType.ToString() + "Bagel") as Texture;
 		bagel.name = bagelType.ToString() + "Bagel" + (++bagelsCount).ToString();
 		bagels.Add(bagel);
@@ -118,6 +153,11 @@ public class GameController : MonoBehaviour {
 	void OnGUI () {
 		GUI.Button(new Rect(0, 0, 100, 100), "Score:\n" + score.ToString());
 		GUI.Button(new Rect(Screen.width - 100, 0, 100, 100), "Missed:\n" + missedBagels.ToString());
+		if (GUI.Button(new Rect(0,Screen.height - 100, 100, 100), "Level:\n" + level.ToString())) {level++; }
+		if (showNextLevel<=0) return;
+		showNextLevel-=Time.deltaTime;
+		GUI.Button( new Rect(Screen.width/4,Screen.height/2 - 200,Screen.width/2,200),"Level " + level);
+		
 	}
 	
 }
--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -5,9 +5,9 @@ public class Settings : MonoBehaviour {
 	
 	//public static Settings instance;
 
-	public static float bagelSpeed = 1f;	//	per second
+	public static float bagelSpeed = 0.6f;	//	per second
 	public static float bagelSpeedIncreasePerLevel = 0.03f;
-	public static float bagelGenerationDelay = 1.1f;
+	public static float bagelGenerationDelay = 2.1f;
 	public static float bagelGenerationDelayDecreasePerLevel = 0.02f;
 	
 	public static int boxAScore = 10;
@@ -17,10 +17,16 @@ public class Settings : MonoBehaviour {
 	public static int boxEScore = 5;
 	public static int missPenalty = -25;
 	
+	public static int bagelsToLevel = 2;
+	public static int[] levelToTypes = new int[] {5,10,15}; //[0] - to 3 types, [1] to 4 ...
+	
+	
+	public static int maxLevel = 50;
+	
 	
 	void Start () {
 		//instance = this;
-		animation.Play("Take 001");
+		//animation.Play("Take 001");
 	}
 	
 }
